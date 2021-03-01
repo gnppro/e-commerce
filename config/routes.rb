9a462760de42c8db2aa41d71a31894e63a96c53b
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2021-03-01 01:29:10 -0600

    🐛 [Fix] bug, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      
+    end
+  end
+
+  get '*page', to: 'pages#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
   root 'pages#index'
-  match '*path', to: 'pages#index', via: :all
+  # match '*path', to: 'pages#index', via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
